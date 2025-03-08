2fb6b9513e142c8574b0d88b1ef5f3e3c0b134b1
onefieryfern <122226601+onefieryfern@users.noreply.github.com>
2025-03-08 23:19:55 +0200

    Set up basic SDL functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
     std::cout << "Hello, World!" << std::endl;
+
+    // Initialise SDL subsystems
+    if (!SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+    {
+        std::cerr << "Failed to initialise SDL: " << SDL_GetError() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    // Create SDL window and associated renderer
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    if (!SDL_CreateWindowAndRenderer("Hello World", 1920, 1080, SDL_WINDOW_RESIZABLE, &window, &renderer))
+    {
+        std::cerr << "Failed to create SDL window and/or renderer: " << SDL_GetError() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    // Program loop
+    while (true)
+    {
+        SDL_Event event;
+        if (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+                break;
+        }
+
+        SDL_Delay(3);
+    }
+
+    // Exit gracefully
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
