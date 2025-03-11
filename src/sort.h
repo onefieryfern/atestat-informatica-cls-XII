db1c9bc43a77d9c004a1c0ba515649012717ae67
onefieryfern <122226601+onefieryfern@users.noreply.github.com>
2025-03-11 14:01:40 +0200

    Change project structure

---
+++ src/sort.h
@@ -0,0 +1,8 @@
+#ifndef ATESTAT_INFORMATICA_CLS_XII_SORT_H
+#define ATESTAT_INFORMATICA_CLS_XII_SORT_H
+
+#include <vector>
+
+void bubble_sort(std::vector<int>& vector);
+
+#endif //ATESTAT_INFORMATICA_CLS_XII_SORT_H
--- src/sort/sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef ATESTAT_INFORMATICA_CLS_XII_SORT_SORT_H
-#define ATESTAT_INFORMATICA_CLS_XII_SORT_SORT_H
-
-#include <vector>
-
-void bubble_sort(std::vector<int>& vector);
-
-#endif //ATESTAT_INFORMATICA_CLS_XII_SORT_SORT_H
