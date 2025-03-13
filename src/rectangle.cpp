c7567c897661004dca8926fee5b8560c728578f2
onefieryfern <122226601+onefieryfern@users.noreply.github.com>
2025-03-13 10:55:31 +0200

    Add `Colours` to describe a rectangle's colours to shorten function parameter lists

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -9,13 +9,26 @@ Rectangle::Rectangle(SDL_FRect sdlFRect, SDL_Color fillColour, SDL_Color outline
     m_outlineColour = outlineColour;
 }
 
+Rectangle::Rectangle(SDL_FRect sdlFRect, Rectangle::Colours colours)
+{
+    m_dimensions = sdlFRect;
+    m_fillColour = colours.fill;
+    m_outlineColour = colours.outline;
+}
+
 SDL_FRect Rectangle::getDimensions() const { return m_dimensions; }
 SDL_Color Rectangle::getFillColour() const { return m_fillColour; }
 SDL_Color Rectangle::getOutlineColour() const { return m_outlineColour; }
+Rectangle::Colours Rectangle::getColours() const { return { getFillColour(), getOutlineColour() }; }
 
 void Rectangle::setDimensions(SDL_FRect sdlFRect) { m_dimensions = sdlFRect; }
 void Rectangle::setFillColour(SDL_Color colour) { m_fillColour = colour; }
 void Rectangle::setOutlineColour(SDL_Color colour) { m_outlineColour = colour; }
+void Rectangle::setColours(Rectangle::Colours colours)
+{
+    setFillColour(colours.fill);
+    setOutlineColour(colours.outline);
+}
 
 /**
  * Draw the rectangle on the current rendering target of the specified renderer
--- src/rectangle.h
+++ src/rectangle.h
@@ -6,20 +6,29 @@
 
 class Rectangle
 {
+public:
+    struct Colours
+    {
+        SDL_Color fill = {0, 0, 0, 0};
+        SDL_Color outline = {0, 0, 0, 0};
+    };
 private:
     SDL_FRect m_dimensions {0, 0, 0, 0};
     SDL_Color m_fillColour {0, 0, 0, 0};
     SDL_Color m_outlineColour {0, 0, 0, 0};
 public:
     Rectangle(SDL_FRect sdlFRect, SDL_Color fillColour, SDL_Color outlineColour);
+    Rectangle(SDL_FRect sdlFRect, Colours colours);
 
     SDL_FRect getDimensions() const;
     SDL_Color getFillColour() const;
     SDL_Color getOutlineColour() const;
+    Colours getColours() const;
 
     void setDimensions(SDL_FRect sdlFRect);
     void setFillColour(SDL_Color colour);
     void setOutlineColour(SDL_Color colour);
+    void setColours(Colours colours);
 
     void draw(SDL_Renderer* renderer) const;
     static void drawRects(SDL_Renderer* renderer, const std::vector<Rectangle>& rects);
