71c6fd1b743b0a59e7e783926f50c7c4130ff863
onefieryfern <122226601+onefieryfern@users.noreply.github.com>
2025-03-29 23:42:02 +0200

    Add new colour constants

--- src/constants.h
+++ src/constants.h
@@ -10,6 +10,10 @@ namespace constants
         constexpr SDL_Color white { 0xff, 0xff, 0xff, SDL_ALPHA_OPAQUE };
         constexpr SDL_Color black { 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE };
         constexpr SDL_Color none { 0x00, 0x00, 0x00, SDL_ALPHA_TRANSPARENT };
+
+        constexpr SDL_Color red { 0xff, 0x00, 0x00, SDL_ALPHA_OPAQUE };
+        constexpr SDL_Color green { 0x00, 0xff, 0x00, SDL_ALPHA_OPAQUE };
+        constexpr SDL_Color blue { 0x00, 0x00, 0xff, SDL_ALPHA_OPAQUE };
     }
 }
 
